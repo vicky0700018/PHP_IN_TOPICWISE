3fc9017e49fb81f8804f04e68c76f6b98a84a749
vicky0700018 <vs2734514@gmail.com>
2025-10-22 20:59:25 +0530

    updated

--- concatenation.php
+++ concatenation.php
@@ -4,10 +4,8 @@ $sName =" tech";
 echo $fName.$sName ." hello" ." vicky";
 
 //basic_sentext
-$string1 = "Hello";
+$string1 = " Hello";
 $string2 = "World!";
 $result = $string1 . " " . $string2;
 echo $result;
-
-
 ?>
