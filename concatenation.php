c41c7a241a61c4aae891630703166521fe19c6ad
vicky0700018 <vs2734514@gmail.com>
2025-10-22 20:57:36 +0530

    updated

--- concatenation.php
+++ concatenation.php
@@ -2,4 +2,12 @@
 $fName ="wscube";
 $sName =" tech";
 echo $fName.$sName ." hello" ." vicky";
+
+//basic_sentext
+$string1 = "Hello";
+$string2 = "World!";
+$result = $string1 . " " . $string2;
+echo $result;
+
+
 ?>
