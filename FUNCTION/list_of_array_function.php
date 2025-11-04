c83d54d6f9ebf9041428aecb7a5db929aed80dfd
vicky0700018 <vs2734514@gmail.com>
2025-11-04 20:49:40 +0530

    updated

---
+++ FUNCTION/list_of_array_function.php
@@ -0,0 +1,46 @@
+<?php
+$car = array(
+    "A" => "car1",
+    "B" => "car2",
+    "C" => "car3",
+    "D" => "car4",
+    "E" => "car5",
+    "F" => "car6",
+);
+$x = 5;
+// if (is_array($car)) {
+//     echo "car is an array";
+// } else {
+//     echo "car is not an array";
+// }
+// var_dump(is_array($x));
+// echo array_search('car4' ,$car);
+// echo "<pre>";
+// $newcar =array_slice($car,1);
+// print_r($newcar);
+// echo "</pre>";
+
+// $poped = array_pop($car);
+// echo($poped);
+// // echo "<pre>";
+// print_r($car);
+// // echo "</pre>";
+echo "<pre>";
+print_r($car);
+echo "</pre>";
+
+// echo "<pre>";
+// print_r(array_keys($car));
+// echo "</pre>";
+// var_dump(array_key_exists("F",$car));
+// var_dump(array_key_exists("G",$car));
+
+// array_push($car, "car7","car8","car9");
+// echo "<pre>";
+// print_r($car);
+// echo "</pre>";
+
+// echo "<pre>";
+// $chucked =array_chunk($car, 2);
+// print_r($chucked);
+// echo "</pre>";
