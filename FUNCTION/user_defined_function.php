b547b3a57ce31ad20b9f86926539c6d9625972af
vicky0700018 <vs2734514@gmail.com>
2025-11-04 20:49:02 +0530

    updated

---
+++ FUNCTION/user_defined_function.php
@@ -0,0 +1,28 @@
+<?php
+function greet (){
+    
+    echo "This is a user defined function";
+    return;
+}
+ greet ();
+?>
+<!-- Syntax of functions in PHP.
+To create a function, we call "function" and give it a name in the same way we give it to variables.
+
+Syntax: function <functionName>() {
+
+ function body.
+
+return if any
+
+}
+
+Arguments: These are the inputs that the function will manipulate -separate multiple inputs with commas.
+
+Syntax: function <functionName>(a, b) {
+
+function body.
+
+return if any
+
+} -->
