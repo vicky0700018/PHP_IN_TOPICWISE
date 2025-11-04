d2bcd8ed0fdf1f00a3f5dcf70a515cd72d698620
vicky0700018 <vs2734514@gmail.com>
2025-11-04 20:48:39 +0530

    updated

---
+++ FUNCTION/pre_define_function.php
@@ -0,0 +1,14 @@
+<?php
+$arr2 = array(
+'name' => 'Bhagirath Giri',
+'company' => 'Wscube tech',
+'contact' => array("12423423","124234534653"),
+'age' => 21,
+'status' => true,
+);
+
+echo "<pre>";
+print_r($arr2);
+echo "</pre>";
+
+?>
