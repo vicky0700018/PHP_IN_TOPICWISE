f69117db2d30da19fc6507ca147c8d4ad06c1861
vicky0700018 <vs2734514@gmail.com>
2025-11-06 23:26:03 +0530

    updated

--- FUNCTION/user_defined_function.php
+++ FUNCTION/user_defined_function.php
@@ -1,28 +1,56 @@
 <?php
-function greet (){
+// function greet (){
     
-    echo "This is a user defined function";
-    return;
+//     echo "This is a user defined function";
+//     return;
+// }
+//  greet ();
+
+
+ 
+function p($data){
+echo "<pre>";
+print_r($data);
+echo "</pre>";
 }
- greet ();
-?>
-<!-- Syntax of functions in PHP.
-To create a function, we call "function" and give it a name in the same way we give it to variables.
+$x = array(
+    "car1",
+    "car2",
+    "car3",
+    "car4",
+    "car5",
+    "car6",
+);
+p($x);
 
-Syntax: function <functionName>() {
+$y = array(
+    "car7",
+    "car8",
+    "car9",
+    "car10",
+    "car11",
+    "car12",
+);
+p($y);
 
- function body.
+// <!-- Syntax of functions in PHP.
+// To create a function, we call "function" and give it a name in the same way we give it to variables.
 
-return if any
+// Syntax: function <functionName>() {
+    
+//     function body.
 
-}
+// return if any
 
-Arguments: These are the inputs that the function will manipulate -separate multiple inputs with commas.
+// }
 
-Syntax: function <functionName>(a, b) {
+// Arguments: These are the inputs that the function will manipulate -separate multiple inputs with commas.
 
-function body.
-
-return if any
-
-} -->
+// Syntax: function <functionName>(a, b) {
+    
+//     function body.
+    
+//     return if any
+    
+// } -->
+    ?>
