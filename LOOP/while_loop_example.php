318e8838df931edb613161d2ac8b9a5e50acb2e9
vicky0700018 <vs2734514@gmail.com>
2025-11-07 22:26:01 +0530

    updated

---
+++ LOOP/while_loop_example.php
@@ -0,0 +1,14 @@
+<?php
+$i = 1;
+$num = 2;
+while($i<=10){
+// echo "Hello<br/>";
+echo $num." x ".$i. " = ".$i*$num."<br/>";
+$i++;
+}
+echo "Loops ends";
+// syntex in while_loop
+// while ('expression'){
+// statement(s) to be executed if
+// expression is true 
+// }
