409189b3ddea68c31b808f10e65d9aff4984115b
vicky0700018 <vs2734514@gmail.com>
2025-11-07 22:25:29 +0530

    updated

---
+++ LOOP/loop.php
@@ -0,0 +1,12 @@
+
+<!-- what is loop:-. A loop is used to repeat a block of code until the specified condition is met. 
+>When similar task is needed to be done again and again.
+>Save time and leads to code reusability.
+
+There are four type of loop 
+1 while loop:-A while loop is to execute a statement or code block repeatedly as long as Condition is true.
+:-Once the Condition becomes false, the loop terminates.
+:-It is also called entry control loop.
+2 do-while-loop:-
+3 for-loop:-
+4 foreach-loop:-
