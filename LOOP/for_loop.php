f489bb130dc298c8947072df0154f96e3a61d6c0
vicky0700018 <vs2734514@gmail.com>
2025-11-08 20:43:28 +0530

    updated

---
+++ LOOP/for_loop.php
@@ -0,0 +1,29 @@
+<?php
+// // for($i=10; $i>0; $i--)
+// for ($i = 0; $i < 10; $i++) {
+//     echo "vicky kumar /t";
+//     echo $i . "<\n>";
+// }
+// echo "loop ends";
+
+
+$fruits = ["Apple", "Banana", "Cherry", "Mango"];
+
+for ($i = 0; $i < count($fruits); $i++) {
+    echo "Fruit: " . $fruits[$i] . "<\n>";
+}
+
+
+// for ($i = 2; $i <= 10; $i += 2) {
+//     echo "Even number: $i . <\n>";
+// }
+
+
+
+
+// // syntex  in for_loop
+// for(initialization; test_condition; +ement/-ement)
+//     {
+// //statement(s) to be executed if test
+// //condition is true 
+// }
