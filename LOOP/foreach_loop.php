edefec1dca68db1c83badd523e705e9500331efc
vicky0700018 <vs2734514@gmail.com>
2025-11-10 23:27:00 +0530

    updated

---
+++ LOOP/foreach_loop.php
@@ -0,0 +1,26 @@
+<?php
+
+use const Dom\SYNTAX_ERR;
+
+$arr=[
+"html"=>2500,
+"css"=>2500,
+"js"=>3000,
+"php"=>4000,
+"laravel"=>8000,
+];
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
+
+
+
+// SYNTAX 
+// foreach ($arr as $key => $value) {
+    
+// }
+foreach ($arr as $course => $fees) {
+    echo "the fees of $course is Rs. $fees <\n>";
+
+}
+?>
