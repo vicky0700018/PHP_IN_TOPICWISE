ff554e986c70a3db11c3b0bc2859b2d9486d07cd
vicky0700018 <vs2734514@gmail.com>
2025-11-14 23:03:51 +0530

    updated

---
+++ BREAK_STATEMENT/break_statement.php
@@ -0,0 +1,26 @@
+<?php
+$i=1;
+while($i<10){
+    if($i == 5){
+        break;
+    }
+    echo $i ."\n";
+    $i++;
+}
+echo"loops end";
+
+
+
+
+
+
+
+
+
+// What is break statement?
+// The break statement is used to break the execution of current loop or switch case.
+// As soon as it is encountered the flow of the program goes to next statement of loop or switch case.
+
+// Syntax: break;       It is also a keyword.
+
+?>
